79b24d317fbac5018fd0adc0ec836611a25ebc00
Param-7 <Paramjotsingh085@gmail.com>
2024-03-14 22:34:11 -0400

    done

--- script.js
+++ script.js
@@ -1,11 +1,24 @@
+
 $(document).ready(function(){
+    // Filter images based on category
     $(".btn").click(function(){
-        var filter = $(this).data("filter");
-        $(".gallery > div").hide();
-        if (filter === "all") {
-            $(".gallery > div").show();
+        var category = $(this).data('filter');
+        if (category === 'all') {
+            $("#gallery > div").show();
         } else {
-            $("#gallery-nature" + filter).hide();
+            $("#gallery > div").hide();
+            $("#gallery-" + category).show();
         }
+    });
+
+    // Lightbox functionality
+    $("#gallery").on("click", "img", function(){
+        var imgSrc = $(this).attr('src');
+        $("#lightbox-overlay").fadeIn();
+        $("#lightbox-content img").attr('src', imgSrc);
+    });
+
+    $("#lightbox-close").click(function(){
+        $("#lightbox-overlay").fadeOut();
     });
 });
